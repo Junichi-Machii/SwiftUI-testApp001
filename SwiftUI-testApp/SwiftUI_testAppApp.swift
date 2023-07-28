a8d90cca54da3f9d538a590ad9fe7bdde2497c34
Junichi-Machii <ronmachii@gmail.com>
2023-07-28 18:18:05 +0900

    Initial Commit

---
+++ SwiftUI-testApp/SwiftUI_testAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_testAppApp.swift
+//  SwiftUI-testApp
+//
+//  Created by Junichi Machii on 2023/07/28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_testAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
